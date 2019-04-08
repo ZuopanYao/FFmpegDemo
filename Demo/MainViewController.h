3bd2602ed7d73543026178ebc5a9dfc003e26693
ZuopanYao <yaozuopan@icloud.com>
2019-04-08 15:34:50 +0800

    init

---
+++ Demo/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  FFmpegDemo
+//
+//  Created by ZuopanYao on 2019.04.08.
+//  Copyright (c) 2019 ZuopanYao/Harvey . All rights reserved.
+//
+//  https://github.com/ZuopanYao/FFmpegDemo.git
+//  this file is part of FFmpegDemo
+//  FFmpegDemo is licenced under the LGPL v3, see lgpl-3.0.txt
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+
+@end
